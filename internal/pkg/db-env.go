9e92c74ebb246db7b60ae01621a7d07d089b4d58
watiko <service@mail.watiko.net>
2020-02-13 02:10:23 +0900

    feat: use migrate cli

--- internal/pkg/db-env.go
+++ internal/pkg/db-env.go
@@ -1,9 +1,5 @@
 package pkg
 
 type DBEnv struct {
-	DbHost string `required:"true" split_words:"true"`
-	DbPort string `required:"true" split_words:"true"`
-	DbUser string `required:"true" split_words:"true"`
-	DbPass string `required:"true" split_words:"true"`
-	DbName string `required:"true" split_words:"true"`
+	DbURL string `required:"true" split_words:"true"`
 }
